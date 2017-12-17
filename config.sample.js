4d6ff6aa5d896a4583642c820b066a2b503ffa59
Peter Tan <petertan1995@gmail.com>
2017-12-16 17:44:03 -0800

    Update config variable name

--- config.sample.js
+++ config.sample.js
@@ -2,6 +2,6 @@ module.exports = {
     api_key: '<Enter API key>',
     api_secret: '<Enter API secret>',
     account_id: '<Enter the Coinbase account id where the program would go to look for the specified wallet address>',
-    ethermine_addr: '<Enter an address for the program to look for transactions>',
+    ethermine_addr_id: '<Enter an address for the program to look for transactions>',
     to_address: '<Enter an email or wallet address where the fund will be transferred to>'
 };
