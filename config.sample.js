42b58317a614fe0f5becda23d059210c179846bb
Peter Tan <petertan1995@gmail.com>
2017-12-16 17:19:53 -0800

    Added a sample config file
    The real config file used for the proram to run has been ignored and omitted for commits.

---
+++ config.sample.js
@@ -0,0 +1,7 @@
+module.exports = {
+    api_key: '<Enter API key>',
+    api_secret: '<Enter API secret>',
+    account_id: '<Enter the Coinbase account id where the program would go to look for the specified wallet address>',
+    ethermine_addr: '<Enter an address for the program to look for transactions>',
+    to_address: '<Enter an email or wallet address where the fund will be transferred to>'
+};
